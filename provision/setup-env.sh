17e5c24094ada48535bc792ff0fc7c3ae097c833
Your Name <angusfz>
2015-02-12 17:51:27 +0800

    q

---
+++ provision/setup-env.sh
@@ -0,0 +1,10 @@
+sudo yum -y update
+sudo yum -y install openssh-server
+sudo yum -y install postfix
+sudo yum -y install cronie
+sudo service postfix start
+sudo chkconfig postfix on
+sudo lokkit -s http -s ssh
+sudo curl -o /tmp/gitlab-7.7.2_omnibus.5.4.2.ci-1.el6.x86_64.rpm https://downloads-packages.s3.amazonaws.com/centos-6.6/gitlab-7.7.2_omnibus.5.4.2.ci-1.el6.x86_64.rpm
+sudo rpm -ivh /tmp/gitlab-7.7.2_omnibus.5.4.2.ci-1.el6.x86_64.rpm
+sudo gitlab-ctl reconfigure
